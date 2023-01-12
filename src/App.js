73f16478c5a8c7c651024bbc4990bb6bf44d745e
Rayncor <sowa332@gmail.com>
2023-01-12 07:03:10 -0800

    sample navbar ref

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { Menu } from './pages/Menu';
 import { Error } from './pages/Error';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div>NAVBAR HERE (ROUTER MUST WRAP) <Link to={'/contact'}>Contact</Link ></div>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/menu' element={<Menu />} />
