7e993bc5cc9b60e951aebbc06220a68583cdf380
Rayncor <sowa332@gmail.com>
2023-01-12 07:00:27 -0800

    quick easy setup of react-router-dom for future reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { Menu } from './pages/Menu';
+import { Error } from './pages/Error';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<Error />} />
         </Routes>
       </Router >
     </div>
